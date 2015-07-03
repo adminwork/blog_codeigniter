a9430f983ed8f89cebb6ae77390bf73e48cbccf2
Ivan <korniyenko.ivan@gmail.com>
2015-07-03 11:01:45 +0300

    ver2.1

--- application/controllers/controller_categories.php
+++ application/controllers/controller_categories.php
@@ -3,10 +3,10 @@
 class Controller_categories extends CI_Controller {
 
     public function index()
-{
+    {
         $id = $this->uri->segment(2);
         $this->load->model('category_models');
-          $this->load->model('post_models');
+        $this->load->model('post_models');
         $data['categories'] =$this->category_models->getAllCategories();
         $data['arrayCategory'] =$this->post_models->getPostById($id);
         $this->load->view('head_view');
--- application/controllers/controller_posts.php
+++ application/controllers/controller_posts.php
@@ -3,10 +3,10 @@
 class Controller_posts extends CI_Controller {
 
     public function index()
-{
+    {
         $id = $this->uri->segment(3);
         $this->load->model('category_models');
-         $this->load->model('post_models');
+        $this->load->model('post_models');
         $data['categories'] =$this->category_models->getAllCategories();
         $data['post'] =$this->post_models->getPostByCategoryId($id);
         $this->load->view('head_view');
