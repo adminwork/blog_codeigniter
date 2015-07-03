ec546b9fc335323eb21a9960a18939121ec66f3b
Ivan <korniyenko.ivan@gmail.com>
2015-07-03 21:25:42 +0300

    ver3.5

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -1,7 +1,7 @@
 <div class="btn-group-vertical" id="sidebar" id="header">
     <?php
     foreach ($categories as $category =>$value){
-        echo "<a href='http://localhost/blog_codeigniter/index.php/category/{$value['id']}' class='btn btn-default'>";
+        echo "<a href='http://localhost/blog_codeigniter/category/{$value['id']}' class='btn btn-default'>";
         echo $value['name'];
         echo '</a>';
     }
